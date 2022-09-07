1f2945a59dd20dba0947aa40b79f86ec91664908
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-08 00:20:16 +0700

    Initial Project

---
+++ src/main/java/com/srs/common/common/mapper/BaseMapper.java
@@ -0,0 +1,127 @@
+package com.srs.common.common.mapper;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author duynt on 2/13/22
+ */
+public abstract class BaseMapper {
+    protected final ModelMapper modelMapper = new ModelMapper();
+
+    @PostConstruct
+    private void configModelMapper() {
+        var stringNullConverter = new Converter<String, String>() {
+            public String convert(MappingContext<String, String> context) {
+                return ObjectUtils.defaultIfNull(context.getSource(), "");
+            }
+        };
+        var integerNullConverter = new Converter<Integer, Integer>() {
+            public Integer convert(MappingContext<Integer, Integer> context) {
+                return ObjectUtils.defaultIfNull(context.getSource(), -1);
+            }
+        };
+        var longNullConverter = new Converter<Long, Long>() {
+            public Long convert(MappingContext<Long, Long> context) {
+                return ObjectUtils.defaultIfNull(context.getSource(), -1L);
+            }
+        };
+        var booleanNullConverter = new Converter<Boolean, Boolean>() {
+            public Boolean convert(MappingContext<Boolean, Boolean> context) {
+                return ObjectUtils.defaultIfNull(context.getSource(), false);
+            }
+        };
+        var floatNullConverter = new Converter<Float, Float>() {
+            @Override
+            public Float convert(MappingContext<Float, Float> context) {
+                return Objects.requireNonNullElse(context.getSource(), -1.0f);
+            }
+        };
+        this.modelMapper.addConverter(stringNullConverter);
+        this.modelMapper.addConverter(integerNullConverter);
+        this.modelMapper.addConverter(longNullConverter);
+        this.modelMapper.addConverter(booleanNullConverter);
+        this.modelMapper.addConverter(floatNullConverter);
+
+        this.modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    /**
+     * Maps {@code source} to {@code destination}.
+     *
+     * @param <S>         {@code source} type
+     * @param <D>         {@code destination} type
+     * @param source      object to map from
+     * @param destination object to map to
+     */
+    public <D, S> D map(final S source, D destination) {
+        if (Objects.isNull(source)) {
+            return null;
+        }
+        modelMapper.map(source, destination);
+        return destination;
+    }
+
+    /**
+     * Maps {@code source} to {@code destination}.
+     * <p>
+     * Note: {@code destination} object must have default constructor with no arguments
+     * </p>
+     *
+     * @param <S>             {@code source} type
+     * @param <D>             {@code destination} type
+     * @param source          object to map from
+     * @param destinationType type to map to
+     * @return new object of {@code destination} type.
+     */
+    public <D, S> D map(final S source, Class<D> destinationType) {
+        return Objects.isNull(source)
+                ? null
+                : modelMapper.map(source, destinationType);
+    }
+
+    /**
+     * <p>
+     * map {@code list of element} from {@code Collection<S> sourceList} into list of mapped object
+     * with {@code D} type by iterating the {@code sourceList}
+     * </p>
+     *
+     * @param sourceList      list of object to map from
+     * @param destinationType type to map to
+     * @param <D>             destination type
+     * @param <S>             {@code sourceList} type
+     * @return list of mapped object with {@code D} type.
+     */
+    public <D, S> List<D> map(final Collection<S> sourceList, Class<D> destinationType) {
+        return Objects.isNull(sourceList)
+                ? null
+                : sourceList.parallelStream().map(source -> this.map(source, destinationType))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * <p>
+     * map {@code list of element} from {@code Collection<S> sourceList} into list of mapped object
+     * with {@code D} type by {@link TypeToken}
+     * </p>
+     *
+     * @param sourceList list of object to map from
+     * @param <D>        destination type
+     * @param <S>        {@code sourceList} type
+     * @return list of mapped object with {@code D} type.
+     */
+    public <D, S> List<D> map(final Collection<S> sourceList) {
+        return modelMapper.map(sourceList, new TypeToken<List<D>>() {
+        }.getType());
+    }
+}
---
+++ src/main/java/com/srs/common/common/util/TimestampUtil.java
@@ -0,0 +1,114 @@
+package com.srs.common.common.util;
+
+import org.flywaydb.core.internal.util.StringUtils;
+
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+/**
+ * @author duynt on 3/30/22
+ */
+public class TimestampUtil {
+    public static final ZoneId DEFAULT_ZONE = ZoneId.of("Asia/Manila");
+    public static final ZoneOffset DEFAULT_OFFSET = ZoneOffset.of("+08:00");
+
+    public static OffsetDateTime parseDatetimeString(String datetime) {
+        return OffsetDateTime.parse(datetime);
+    }
+
+    public static OffsetTime parseTimeString(String time) {
+        return OffsetTime.parse(time).withSecond(0);
+    }
+
+    public static String stringifyDatetime(OffsetDateTime datetime, boolean includeTime) {
+        if (datetime == null) {
+            return "";
+        }
+
+        if (includeTime) {
+            var offset = datetime.getOffset().toString().equals("Z")
+                    ? "+00:00" : datetime.getOffset().toString();
+
+            return String.format("%s-%s-%sT%s:%s:%s",
+                    datetime.getYear(),
+                    StringUtils.leftPad(String.valueOf(datetime.getMonthValue()), 2, '0'),
+                    StringUtils.leftPad(String.valueOf(datetime.getDayOfMonth()), 2, '0'),
+                    StringUtils.leftPad(String.valueOf(datetime.getHour()), 2, '0'),
+                    StringUtils.leftPad(String.valueOf(datetime.getMinute()), 2, '0'),
+                    StringUtils.leftPad(String.valueOf(datetime.getSecond()), 2, '0') + offset);
+        } else {
+            return String.format("%s-%s-%s",
+                    datetime.getYear(),
+                    StringUtils.leftPad(String.valueOf(datetime.getMonthValue()), 2, '0'),
+                    StringUtils.leftPad(String.valueOf(datetime.getDayOfMonth()), 2, '0'));
+        }
+    }
+
+    public static String stringifyTime(OffsetTime time) {
+        if (time == null) {
+            return "";
+        }
+
+        var offset = time.getOffset().toString().equals("Z")
+                ? "+00:00" : time.getOffset().toString();
+
+        return String.format("%s:%s:%s",
+                StringUtils.leftPad(String.valueOf(time.getHour()), 2, '0'),
+                StringUtils.leftPad(String.valueOf(time.getMinute()), 2, '0'),
+                StringUtils.leftPad(String.valueOf(time.getSecond()), 2, '0')) + offset;
+    }
+
+    public static OffsetDateTime now() {
+        return OffsetDateTime.now().withNano(0).withOffsetSameInstant(DEFAULT_OFFSET);
+    }
+
+    public static OffsetDateTime nowAtDefaultTimezone() {
+        return OffsetDateTime.now().withNano(0);
+    }
+
+    public static OffsetDateTime beginningOfDate(OffsetDateTime date) {
+        return date.withHour(0).withMinute(0).withSecond(0).withNano(0);
+    }
+
+    public static OffsetDateTime endOfDate(OffsetDateTime date) {
+        return date.withHour(23).withMinute(59).withSecond(59).withNano(0);
+    }
+
+    public static boolean isBetween(OffsetDateTime theDate, OffsetDateTime lower, OffsetDateTime upper) {
+        return (theDate.isAfter(lower) || theDate.isEqual(lower)) && theDate.isBefore(upper);
+    }
+
+    public static OffsetDateTime endOfToday() {
+        return endOfDate(now());
+    }
+
+    public static OffsetDateTime beginningOfToday() {
+        return beginningOfDate(now());
+    }
+
+    public static OffsetDateTime getFirstDayOfQuarter(OffsetDateTime date) {
+        var month = date.getMonthValue();
+        int quarter = (int) Math.ceil(month / 3.0f);
+
+        return beginningOfDate(date
+                .withMonth(1 + (quarter - 1) * 3)
+                .withDayOfMonth(1))
+                .withOffsetSameLocal(TimestampUtil.DEFAULT_OFFSET);
+    }
+
+    public static OffsetDateTime getLastDayOfQuarter(OffsetDateTime date) {
+        var month = date.getMonthValue();
+        var quarter = (int) Math.ceil(month / 3.0f);
+
+        var lastDate = date.withMonth((3 * quarter + 1) % 12)
+                .withDayOfMonth(1)
+                .minusDays(1)
+                .plusYears(quarter == 4 ? 1 : 0);
+
+        return endOfDate(lastDate.withOffsetSameLocal(TimestampUtil.DEFAULT_OFFSET));
+    }
+
+
+}
