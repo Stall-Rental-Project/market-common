cec1b7df77af5eee8644ad14587fa05c46a0e41e
ngohoang2110 <hoanggg2110@gmail.com>
2022-09-07 21:36:26 +0700

    Initial Project

---
+++ src/main/java/com/srs/common/MarketCommonApplication.java
@@ -0,0 +1,13 @@
+package com.srs.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srs/common/MarketCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
